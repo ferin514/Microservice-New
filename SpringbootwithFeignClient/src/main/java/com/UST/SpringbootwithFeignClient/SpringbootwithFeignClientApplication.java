9b78a9f945fbe19469dc5928581c5cd54710cb01
meenakshi sajeevan <meenakshisajeevan2000@gmail.com>
2023-05-18 16:41:53 +0530

    first commit

---
+++ SpringbootwithFeignClient/src/main/java/com/UST/SpringbootwithFeignClient/SpringbootwithFeignClientApplication.java
@@ -0,0 +1,13 @@
+package com.UST.SpringbootwithFeignClient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootwithFeignClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootwithFeignClientApplication.class, args);
+	}
+
+}
---
+++ SpringbootwithFeignClient/src/test/java/com/UST/SpringbootwithFeignClient/SpringbootwithFeignClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.UST.SpringbootwithFeignClient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootwithFeignClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
